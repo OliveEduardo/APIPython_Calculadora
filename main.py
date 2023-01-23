dfc4675f73811c174558a0541b5e645899ff9efe
OliveEduardo <114627600+OliveEduardo@users.noreply.github.com>
2023-01-23 18:13:54 -0300

    Banco de Dados

--- main.py
+++ main.py
@@ -1,31 +1,18 @@
-from fastapi import FastAPI, Path
-import os
-app = FastAPI(title='Calculadora', description='Simples calculadora em Python e APIrest',version='1.0.0')
+import uvicorn
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi import FastAPI
+from routes.api import router as api_router
 
-@app.get('/calculadora')
-def homepage():
-    return {"Bem": "Vindo"}
+app = FastAPI()
 
+origins = ["http://localhost:5000"]
 
-@app.get('/calculadora/soma/{n1}/{n2}')
-def Operacao_soma(n1: int = Path(description='Variavel 1 do tipo inteiro'),  n2: int = Path(description='Variavel 2 do tipo inteiro')):
-    soma = n1 + n2
-    return soma
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-
-@app.get('/calculadora/sub/{n1}/{n2}')
-def Operacao_Subtraao(n1: int = Path(description='Variavel 1 do tipo inteiro'),  n2: int = Path(description='Variavel 2 do tipo inteiro')):
-    sub = n1 - n2
-    return sub
-
-
-@app.get('/calculadora/mult/{n1}/{n2}')
-def Operacao_Multiplicacao(n1: int = Path(description='Variavel 1 do tipo inteiro'),  n2: int = Path(description='Variavel 2 do tipo inteiro')):
-    mult = n1 * n2
-    return mult
-
-
-@app.get('/calculadora/div/{n1}/{n2}')
-def Operacao_Divisao(n1: int = Path(description='Variavel 1 do tipo inteiro'),  n2: int = Path(description='Variavel 2 do tipo inteiro')):
-    div = n1 / n2
-    return div
+app.include_router(api_router)
